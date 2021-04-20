34d43d9abf671b79a97c627bc1fd85989d5d904a
Kevenn Caterinque <k.douglascaterinque@gmail.com>
2021-04-20 08:38:07 -0300

    fixed side menu item styles

--- resources/views/components/admin-side-menu-item.blade.php
+++ resources/views/components/admin-side-menu-item.blade.php
@@ -1,15 +1,7 @@
 @props(['title', 'route'])
 
-@php
-    $classes = 'py-1 px-3 rounded-lg hover:bg-gray-600 ';
-    $classes .= request()->routeIs($route)
-                ? 'bg-gray-800 text-white'
-                : 'text-gray-300';
-@endphp
-
-
-<li {{ $attributes->merge(['class' => $classes]) }}>
-    <a href="{{ route($route) }}" class="inline-block w-full">
+<li class="side-menu-item {{ request()->routeIs($route) ? 'active' : '' }}">
+    <a href="{{ route($route) }}" class="inline-block w-full py-2 px-3">
         {{ $title }}
     </a>
 </li>
