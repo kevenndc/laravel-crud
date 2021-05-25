c2b590779ff52ea5970fa184f7d3e256dc8ca845
Kevenn Caterinque <k.douglascaterinque@gmail.com>
2021-05-25 07:53:40 -0300

    finished message animation

--- resources/views/layouts/message.blade.php
+++ resources/views/layouts/message.blade.php
@@ -8,11 +8,12 @@
                 right: 2rem;
             }
         }
-        #notification-message {
+        .message-animation {
             animation: slideLeft 300ms ease;
+            animation-iteration-count: 1;
         }
     </style>
-    <div class="fixed top-4 z-10 shadow-md border-2 bg-white p-4 max-w-md animate-slideLeft" id="notification-message">
+    <div class="fixed top-4 right-8 z-10 shadow-md border-2 bg-white p-4 max-w-md message-animation transition-opacity duration-200" id="notification-message">
         <x-dynamic-component :component="$messageType">
             <x-slot name="closeButton">
                 <span class="absolute right-0 text-gray-600 cursor-pointer" onclick="closeMessage()">
@@ -24,7 +25,9 @@
     </div>
     <script>
         function closeMessage() {
-            document.getElementById('notification-message').style.display = 'none';
+            const message = document.getElementById('notification-message');
+            message.classList.add('opacity-0');
+            setTimeout(() => message.style.display = 'none', 200);
         }
         setTimeout(closeMessage, 5000);
     </script>
